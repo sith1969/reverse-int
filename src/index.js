3becab2613f9c6ddbd40429495659cba4297bbbc
sith1969 <sith@tut.by>
2020-10-16 09:58:37 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+'use strict';
 module.exports = function reverse (n) {
-  
-}
+    if(n < 0) {n = Math.abs(n);}
+    if(n < 0) {n = Math.abs(n);}
+    n = `${n}`.split('').reverse().join('');
+    return +n;  
+};
+
